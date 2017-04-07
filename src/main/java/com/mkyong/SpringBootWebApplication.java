a05d66c44fce66323ab33ea5e4eb22a3c6b78480
brandon.duffy <brandon.duffy@stillwater.com>
2017-04-07 15:20:43 -0500

    initial commit

---
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -0,0 +1,24 @@
+package com.mkyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+<head>
+
+<c:url value="/css/main.css" var="jstlCss" />
+<link href="${jstlCss}" rel="stylesheet" />
+
+</head>
+<body>
+
+	<h5>${message}</h5>
+
+</body>
+
+</html>
